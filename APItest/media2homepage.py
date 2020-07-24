4c3760a50c5ec73306244a46c9ee716c82cb0169
wenjieher <wenjieher@gmail.com>
2020-07-24 15:11:53 +0800

    1
    2

--- APItest/media2homepage.py
+++ APItest/media2homepage.py
@@ -21,7 +21,15 @@ class TestMethod(unittest.TestCase):
         self.taskid1 = task1.json()["data"]["id"]
         a = self.session.post("https://hongkou-gateway.shmedia.tech/api/jingle/cms/organization-content/personal/add", data="{\"channelId\":null,\"modelId\":\"310109-1\",\"txt\":\"<p><span style=\\\"background-color: rgb(255, 255, 255); color: rgb(51, 51, 51);\\\">article</span></p>\",\"reporters\":[],\"title\":\"test article\",\"missions\":[],\"clues\":[],\"coverImages\":[],\"coverImageType\":\"multi-image\",\"link\":\"\"}", headers={'Content-Type': 'application/json', 'accessToken': self.access_Token})
         self.articleid = a.json()["data"]["id"]
-
+        articlesend = self.session.post("https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/autoAdd", data="{\"modelId\":\"310109-1\",\"channelId\":\"b08f8f2bf3344bedb88217287907d9fc\",\"txt\":\"<p>test</p>\",\"lock\":false,\"settings\":{\"configUI\":{\"shortcuts\":{\"enable\":true},\"panels\":{\"enable\":true},\"comments\":{\"enable\":true},\"hasLogin\":{\"enable\":true},\"header\":{\"enable\":true,\"title\":\"\",\"close\":\"enable\",\"voice\":\"enable\",\"more\":\"enable\"},\"footer\":{\"enable\":true,\"input\":\"enable\",\"favorite\":\"enable\",\"like\":\"enable\"},\"moreBox\":{\"share\":\"enable\",\"theme\":\"enable\",\"report\":\"enable\",\"copyLink\":\"enable\"}},\"appConfig\":{\"hasLogin\":{\"enable\":false}},\"configShare\":{\"title\":\"\",\"summary\":\"\",\"link\":\"\",\"imageUrl\":\"\",\"imageId\":\"\"}},\"link\":\"\",\"coverImages\":[],\"coverImageType\":\"multi-image\",\"title\":\"test article send\"}", headers={'Content-Type': 'application/json', 'accessToken': self.access_Token})
+        self.articlesend = articlesend.json()["data"]["id"]
+        articledelete = self.session.post("https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/autoAdd",data="{\"modelId\":\"310109-1\",\"channelId\":\"b08f8f2bf3344bedb88217287907d9fc\",\"txt\":\"<p>test</p>\",\"lock\":false,\"settings\":{\"configUI\":{\"shortcuts\":{\"enable\":true},\"panels\":{\"enable\":true},\"comments\":{\"enable\":true},\"hasLogin\":{\"enable\":true},\"header\":{\"enable\":true,\"title\":\"\",\"close\":\"enable\",\"voice\":\"enable\",\"more\":\"enable\"},\"footer\":{\"enable\":true,\"input\":\"enable\",\"favorite\":\"enable\",\"like\":\"enable\"},\"moreBox\":{\"share\":\"enable\",\"theme\":\"enable\",\"report\":\"enable\",\"copyLink\":\"enable\"}},\"appConfig\":{\"hasLogin\":{\"enable\":false}},\"configShare\":{\"title\":\"\",\"summary\":\"\",\"link\":\"\",\"imageUrl\":\"\",\"imageId\":\"\"}},\"link\":\"\",\"coverImages\":[],\"coverImageType\":\"multi-image\",\"title\":\"test article send\"}",headers={'Content-Type': 'application/json', 'accessToken': self.access_Token})
+        self.articledelete = articledelete.json()["data"]["id"]
+        send = self.session.post("https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/page", data="{\"isDeleted\":false,\"recursion\":false,\"pageNo\":1,\"pageSize\":20,\"channelId\":\"b08f8f2bf3344bedb88217287907d9fc\"}",headers={'Content-Type': 'application/json', 'accessToken': self.access_Token})
+        self.articleid1 = send.json()["data"]["records"][0]["id"]
+        delete1 = self.session.post("https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/page", data="{\"isDeleted\":false,\"recursion\":false,\"pageNo\":1,\"pageSize\":20,\"channelId\":\"b08f8f2bf3344bedb88217287907d9fc\"}", headers={'Content-Type': 'application/json', 'accessToken': self.access_Token})
+        self.delete1 = delete1.json()["data"]["records"][1]["id"]
+        print(self.articleid1)
 
     @classmethod
     def tearDown(self):
@@ -788,6 +796,350 @@ class TestMethod(unittest.TestCase):
         print(result1)
         self.assertEqual("success", result1)
         self.assertEqual(0, result2)
+
+    def test041(self):
+        """机构文稿列表organization-content/page接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/organization-content/page"
+
+        payload = "{\"pageNo\":1,\"pageSize\":10,\"isDeleted\":false,\"orderBy\":\"create_desc\"}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test042(self):
+        """内容管理渠道列表target/tree接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/publish/target/tree"
+
+        payload = "{\"enableFlag\":1,\"systemType\":2}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test043(self):
+        """内容管理列表content/page接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/page"
+
+        payload = "{\"isDeleted\":false,\"recursion\":false,\"pageNo\":1,\"pageSize\":20,\"channelId\":\"b08f8f2bf3344bedb88217287907d9fc\"}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test044(self):
+        """文稿提交审核content/check接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/check"
+
+        payload = {'contents':[{'id':self.articleid}]}
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test045(self):
+        """查看操作日志content/findEditRecordList接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/findEditRecordList"
+
+        payload = "{\"channelContentId\":\"5ccbb0aa7095433ca9252a7462d0593f\"}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test046(self):
+        """查看审核日志record/list接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/workflow/record/list"
+
+        payload = {'contentId':self.articleid}
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test047(self):
+        """提交文稿审核content/check接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/check"
+
+        payload = {'contents':[{'id':self.articleid1}]}
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test048(self):
+        """发布审核列表content/page接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/page"
+
+        payload = "{\"pageNo\":1,\"pageSize\":10,\"status\":1,\"orderBy\":\"create_desc\"}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test049(self):
+        """一审content/check接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/check"
+
+        payload = {'contents': [{'id': self.articleid1}]}
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test050(self):
+        """二审content/check接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/check"
+
+        payload = {'contents': [{'id': self.articleid1}]}
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test051(self):
+        """三审发布content/check接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/check"
+
+        payload = {'contents': [{'id': self.articleid1}]}
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test052(self):
+        """获取评论管理列表review/list接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/external/article/review/list"
+
+        payload = "{\"pageNo\":1,\"pageSize\":10,\"sourceType\":\"2\",\"memberId\":\"\"}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test053(self):
+        """回收站列表content/page接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/page"
+
+        payload = "{\"pageNo\":1,\"pageSize\":10,\"isDeleted\":true}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test054(self):
+        """删除草稿文稿content/delete接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/content/delete"
+
+        payload = [{'id':self.delete1}]
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': 'dc755e05215bdd7e3648afe954116ee9'
+        }
+
+        response = requests.request("POST", url, headers=headers, json=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test055(self):
+        """站点信息site/get接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/site/get"
+
+        payload = "{}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test056(self):
+        """保存站点信息site/update信息"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/site/update"
+
+        payload = "{\"id\":\"310109\",\"name\":\"虹口\",\"shortName\":\"虹口\",\"protocol\":\"https://\",\"domain\":\"hongkou-gateway.shmedia.tech\",\"releasePath\":\"/public/app_hk\",\"path\":\"app_hk\",\"staticSuffix\":\".html\",\"workflowId\":\"ID_27_001\",\"afterCheckFlag\":\"0\",\"recycleFlag\":\"1\",\"staticFlag\":\"1\",\"deleted\":false}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload.encode())
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test057(self):
+        """栏目管理列表channel/list接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/channel/list"
+
+        payload = "{\"recursion\":true,\"parentId\":\"310109-3\"}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': self.access_Token
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+    def test058(self):
+        """模型管理model/list接口"""
+        url = "https://hongkou-gateway.shmedia.tech/api/jingle/cms/model/list"
+
+        payload = "{}"
+        headers = {
+            'Content-Type': 'application/json',
+            'accessToken': 'dc755e05215bdd7e3648afe954116ee9'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+        result1 = response.json()['msg']
+        result2 = response.json()['code']
+        print(result1)
+        self.assertEqual("success", result1)
+        self.assertEqual(0, result2)
+
+
 
 
 
